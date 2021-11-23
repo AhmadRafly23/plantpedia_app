8615214882b9f0b10115d8c0902b063ecaec1dc4
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2021-11-23 19:00:00 +0700

    add

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -6,7 +6,7 @@ class Home extends React.Component {
         return (
             <div className="container">
                 <div className="container-left">
-                    <h1><span>Plants</span> make life better</h1>
+                    <h1><span className="plant">Plants</span> make life better</h1>
                     <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
                         tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
                         quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo

e16a1957f2a88e21de6df11725ae85bb232ddeaf
AhmadRafly23 <pradanaahmadrafly@gmail.com>
2021-12-26 23:16:40 +0700

    revisi

--- src/About/About.js
+++ src/About/About.js
@@ -1,44 +1,48 @@
 import React from "react";
 import "./About.css";
-import {FiMail} from "react-icons/fi"
-import {HiOutlineLocationMarker} from "react-icons/hi"
-import {AiOutlinePhone} from "react-icons/ai"
+import { FiMail } from "react-icons/fi";
+import { HiOutlineLocationMarker } from "react-icons/hi";
+import { AiOutlinePhone } from "react-icons/ai";
 
-class About extends React.Component{
-    render(){
-        return(
-            <div className="container">
-                <div className="about">
-                    <img src="/foto.jpg" alt="profile"/>
-                    <p>Ahmad Rafly Pradana</p>
-                    <div className="student">Student</div>
-                    <div className="line"></div>
-                    <div className="location">
-                        <p className="info">
-                            <HiOutlineLocationMarker className = "icon" size="18px" color="#484d49" />
-                            Location
-                        </p>
-                        <p>Sleman, Yogyakarta</p>
-                    </div>
-                    <div className="location">
-                        <p className="info">
-                            <AiOutlinePhone className = "icon" size="18px" color="#484d49" />
-                           Number 
-                        </p>
-                        <p>+6283-820-653-721</p>
-                    </div>
-                    <div className="location">
-                        <p className="info">
-                            <FiMail className = "icon" size="18px" color="#484d49" />
-                            Email
-                        </p>
-                        <p>arafly@gmail.com</p>
-                    </div>
-                    <p className="prak">Praktikum Mobile Device Programming</p>
-                </div>
-            </div>
-        );
-    }
+class About extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="about">
+          <img src="/foto.jpg" alt="profile" />
+          <p>Shinji Kagawa</p>
+          <div className="student">Manager</div>
+          <div className="line"></div>
+          <div className="location">
+            <p className="info">
+              <HiOutlineLocationMarker
+                className="icon"
+                size="18px"
+                color="#484d49"
+              />
+              Location
+            </p>
+            <p>Sleman, Yogyakarta</p>
+          </div>
+          <div className="location">
+            <p className="info">
+              <AiOutlinePhone className="icon" size="18px" color="#484d49" />
+              Number
+            </p>
+            <p>+6283-820-653-721</p>
+          </div>
+          <div className="location">
+            <p className="info">
+              <FiMail className="icon" size="18px" color="#484d49" />
+              Email
+            </p>
+            <p>arafly@gmail.com</p>
+          </div>
+          <p className="prak">PlantPedia App</p>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default About;
